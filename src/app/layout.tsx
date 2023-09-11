156380a9ad8e4325da6413046e0f9edfe2757bd5
franco <franco@wildaudience.com>
2023-09-11 15:02:38 -0300

    add title on general layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,25 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Reading list',
-  description: 'Reading list for the modern web developer',
-}
+  title: "Reading list",
+  description: "Create a reading list",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="es">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <h1 className="text-center p-8 text-[3rem]">Reading Book List</h1>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center w-full ">
-      <h1 className="p-8 text-[3rem]">Reading Book List</h1>
       <Books books={data} />
     </main>
   );
